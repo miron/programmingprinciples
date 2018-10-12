30986114f161aecf356c5b41dcc735182e7e0987
miron <m.iron@icloud.com>
2018-10-12 09:02:31 +0200

    changed vector to int min and int max for clarity

--- guess.cpp
+++ guess.cpp
@@ -10,22 +10,22 @@ return true;
 int main()
 {
    char a;
-   vector<int> number = {1,100};
-           cout << "Think of a number between 1 and 100. Ready? Press \"r\" and enter.\n";
+   int min = 1;
+   int max = 100;
+           cout << "Think of a number between " << min << " and " << max << ". Ready? Press \"r\" and enter.\n";
            cin >> a; 
-           cout << "Is it <= " << number[1]/2 <<" (y/n)?\n";
+           cout << "Is it <= " << max/2 <<" (y/n)?\n";
 
    while (cin>>a){
-       if(number[0]==number[1])
-           cout << "The number you thought of is " << number[0] << "\n";
+       if(min==max)
+           cout << "The number you thought of is " << min << "\n";
        else if(a=='y'){
-           number[1]=number[1]/2;
-           cout << "Is it <= " << number[1]/2 <<" (y/n)?\n";
+           max=max/2;
+           cout << "Is it <= " << max/2 <<" (y/n)?\n";
         }   
        else if(a=='n'){
-           number[0]=50+number[0]/2;
-           cout << "Is it <= " << (number[1]+number[0])/2 <<" (y/n)?\n";
-           if(a==')
+           min=max/2+min/2;
+           cout << "Is it <= " << (min+max)/2 <<" (y/n)?\n";
         }
    } 
 
