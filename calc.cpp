ac9e62907b72321aa6114fffa3a5ea3573c8e6cb
miron <m.iron@icloud.com>
2018-10-22 14:40:05 +0200

    trying read integer and if fail string next

--- calc.cpp
+++ calc.cpp
@@ -4,12 +4,19 @@ int main()
     double val1, val2, result;
     char operat;
     string text;
+    vector<string> numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    vector<string> digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
     cout << "Input value 1\n";
     cin >> val1;
     cout << "Input value 2\n";
     cin >> val2;
     cout << "Input operator\n";
     cin >> operat;
+    for(int i=0; i<numbers.size(); ++i){
+       if(numbers[i]==val1) val1=i;
+       else if(digits[i]==val1) val1=numbers[i];
+    }
+  
     switch(operat){
     case '+':
          text="The sum of "; 
